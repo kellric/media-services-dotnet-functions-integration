02d154c0fb34d42e2b42168c021b5ffd8e91e66e
Richard Edwards <richard.edwards@dimensional.com>
2019-05-01 12:58:45 -0500

    adding progressive locator

--- media-functions-for-logic-app/functions/publish-asset.cs
+++ media-functions-for-logic-app/functions/publish-asset.cs
@@ -8,6 +8,7 @@ Input:
 {
     "assetId" : "nb:cid:UUID:2d0d78a2-685a-4b14-9cf0-9afb0bb5dbfc", // Mandatory, Id of the source asset
     "preferredSE" : "default" // Optional, name of Streaming Endpoint if a specific Streaming Endpoint should be used for the URL outputs
+    "locatorType" "OnDemandOrigin " // OnDemandOrigin  or sas locator type 
 }
 
 Output:
@@ -18,43 +19,36 @@ Output:
 }
 */
 
-
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
-using Newtonsoft.Json;
-using Microsoft.WindowsAzure.MediaServices.Client;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.MediaServices.Client;
+using Newtonsoft.Json;
 
-
-namespace media_functions_for_logic_app
-{
-    public static class publish_asset
-    {
+namespace media_functions_for_logic_app {
+    public static class publish_asset {
         // Field for service context.
         private static CloudMediaContext _context = null;
 
-        [FunctionName("publish-asset")]
-        public static async Task<object> Run([HttpTrigger(WebHookType = "genericJson")]HttpRequestMessage req, TraceWriter log)
-        {
+        [FunctionName ("publish-asset")]
+        public static async Task<object> Run ([HttpTrigger (WebHookType = "genericJson")] HttpRequestMessage req, TraceWriter log) {
             {
-                log.Info($"Webhook was triggered!");
+                log.Info ($"Webhook was triggered!");
 
-                string jsonContent = await req.Content.ReadAsStringAsync();
-                dynamic data = JsonConvert.DeserializeObject(jsonContent);
+                string jsonContent = await req.Content.ReadAsStringAsync ();
+                dynamic data = JsonConvert.DeserializeObject (jsonContent);
 
-                log.Info(jsonContent);
+                log.Info (jsonContent);
 
-                if (data.assetId == null)
-                {
+                if (data.assetId == null) {
                     // for test
                     // data.assetId = "nb:cid:UUID:c0d770b4-1a69-43c4-a4e6-bc60d20ab0b2";
-                    return req.CreateResponse(HttpStatusCode.BadRequest, new
-                    {
+                    return req.CreateResponse (HttpStatusCode.BadRequest, new {
                         error = "Please pass asset ID in the input object (assetId)"
                     });
                 }
@@ -63,69 +57,68 @@ namespace media_functions_for_logic_app
                 string smoothUrl = "";
                 string pathUrl = "";
                 string preferredSE = data.preferredSE;
+                string locatorType = data.locatorType;
 
-                MediaServicesCredentials amsCredentials = new MediaServicesCredentials();
-                log.Info($"Using Azure Media Service Rest API Endpoint : {amsCredentials.AmsRestApiEndpoint}");
+                MediaServicesCredentials amsCredentials = new MediaServicesCredentials ();
+                log.Info ($"Using Azure Media Service Rest API Endpoint : {amsCredentials.AmsRestApiEndpoint}");
 
-                try
-                {
-                    AzureAdTokenCredentials tokenCredentials = new AzureAdTokenCredentials(amsCredentials.AmsAadTenantDomain,
-                                                    new AzureAdClientSymmetricKey(amsCredentials.AmsClientId, amsCredentials.AmsClientSecret),
-                                                    AzureEnvironments.AzureCloudEnvironment);
+                try {
+                    AzureAdTokenCredentials tokenCredentials = new AzureAdTokenCredentials (amsCredentials.AmsAadTenantDomain,
+                        new AzureAdClientSymmetricKey (amsCredentials.AmsClientId, amsCredentials.AmsClientSecret),
+                        AzureEnvironments.AzureCloudEnvironment);
 
-                    AzureAdTokenProvider tokenProvider = new AzureAdTokenProvider(tokenCredentials);
+                    AzureAdTokenProvider tokenProvider = new AzureAdTokenProvider (tokenCredentials);
 
-                    _context = new CloudMediaContext(amsCredentials.AmsRestApiEndpoint, tokenProvider);
-
+                    _context = new CloudMediaContext (amsCredentials.AmsRestApiEndpoint, tokenProvider);
 
                     // Get the asset
                     string assetid = data.assetId;
-                    var outputAsset = _context.Assets.Where(a => a.Id == assetid).FirstOrDefault();
+                    var outputAsset = _context.Assets.Where (a => a.Id == assetid).FirstOrDefault ();
 
-                    if (outputAsset == null)
-                    {
-                        log.Info($"Asset not found {assetid}");
+                    if (outputAsset == null) {
+                        log.Info ($"Asset not found {assetid}");
 
-                        return req.CreateResponse(HttpStatusCode.BadRequest, new
-                        {
+                        return req.CreateResponse (HttpStatusCode.BadRequest, new {
                             error = "Asset not found"
                         });
                     }
 
                     // publish with a streaming locator (100 years)
-                    IAccessPolicy readPolicy2 = _context.AccessPolicies.Create("readPolicy", TimeSpan.FromDays(365 * 100), AccessPermissions.Read);
-                    ILocator outputLocator2 = _context.Locators.CreateLocator(LocatorType.OnDemandOrigin, outputAsset, readPolicy2);
 
-                    var publishurlsmooth = MediaServicesHelper.GetValidOnDemandURI(_context, outputAsset, preferredSE);
-                    var publishurlpath = MediaServicesHelper.GetValidOnDemandPath(_context, outputAsset, preferredSE);
+                    if (locatorType == "OnDemandOrigin ") {
+                        IAccessPolicy readPolicy2 = _context.AccessPolicies.Create ("readPolicy", TimeSpan.FromDays (365 * 100), AccessPermissions.Read);
+                        ILocator outputLocator2 = _context.Locators.CreateLocator (LocatorType.OnDemandOrigin, outputAsset, readPolicy2);
 
-                    if (outputLocator2 != null && publishurlsmooth != null)
-                    {
-                        smoothUrl = publishurlsmooth.ToString();
-                        playerUrl = "https://ampdemo.azureedge.net/?url=" + HttpUtility.UrlEncode(smoothUrl);
-                        log.Info($"smooth url : {smoothUrl}");
+                        var publishurlsmooth = MediaServicesHelper.GetValidOnDemandURI (_context, outputAsset, preferredSE);
+                        var publishurlpath = MediaServicesHelper.GetValidOnDemandPath (_context, outputAsset, preferredSE);
+
+                        if (outputLocator2 != null && publishurlsmooth != null) {
+                            smoothUrl = publishurlsmooth.ToString ();
+                            playerUrl = "https://ampdemo.azureedge.net/?url=" + HttpUtility.UrlEncode (smoothUrl);
+                            log.Info ($"smooth url : {smoothUrl}");
+                        }
+
+                        if (outputLocator2 != null && publishurlpath != null) {
+                            pathUrl = publishurlpath.ToString ();
+                            log.Info ($"path url : {pathUrl}");
+                        }
                     }
+                    if (locatorType == "sas ") {
+                         IAccessPolicy readPolicy3 = _context.AccessPolicies.Create ("readPolicy", TimeSpan.FromDays (365 * 100), AccessPermissions.Read);
+                        ILocator outputLocator3 = _context.Locators.CreateLocator (LocatorType.SAS, outputAsset, readPolicy2);
 
-                    if (outputLocator2 != null && publishurlpath != null)
-                    {
-                        pathUrl = publishurlpath.ToString();
-                        log.Info($"path url : {pathUrl}");
                     }
+                } catch (Exception ex) {
+                    string message = ex.Message + ((ex.InnerException != null) ? Environment.NewLine + MediaServicesHelper.GetErrorMessage (ex) : "");
+                    log.Info ($"ERROR: Exception {message}");
+                    return req.CreateResponse (HttpStatusCode.InternalServerError, new { error = message });
                 }
 
-                catch (Exception ex)
-                {
-                    string message = ex.Message + ((ex.InnerException != null) ? Environment.NewLine + MediaServicesHelper.GetErrorMessage(ex) : "");
-                    log.Info($"ERROR: Exception {message}");
-                    return req.CreateResponse(HttpStatusCode.InternalServerError, new { error = message });
-                }
-
-                log.Info($"");
-                return req.CreateResponse(HttpStatusCode.OK, new
-                {
+                log.Info ($"");
+                return req.CreateResponse (HttpStatusCode.OK, new {
                     playerUrl = playerUrl,
-                    smoothUrl = smoothUrl,
-                    pathUrl = pathUrl
+                        smoothUrl = smoothUrl,
+                        pathUrl = pathUrl
                 });
             }
         }
